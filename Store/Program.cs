3687eabba0d42975f7f85d892fdb3b16e93b5d7c
Kevin Nathanailidis <kevin.nathanailidis@elev.ga.ntig.se>
2024-10-08 12:56:54 +0200

    dsgfhsgd

--- Store/Program.cs
+++ Store/Program.cs
@@ -1,35 +1,60 @@
-﻿int i = 0;
-while (i < 10)
+﻿int SpelarPengar = 100;
+
+while (SpelarPengar > 0)
 {
-Console.WriteLine("Hej");
-    i++;
-}
+    Console.WriteLine($"Du har {SpelarPengar} Kronor kvar");
 
-for (int j=0; j < 10; j++)
-{
-    Console.WriteLine("Hej");
-}
+    Console.WriteLine("Vad vill du köpa");
+    Console.WriteLine("1. Tzatziki 10kr");
+    Console.WriteLine("2. Gyros 20kr");
+    Console.WriteLine("3. Souvlaki 30kr");
 
-
-bool success = false;
-    int ageNum = 0;
-
-while (success != true)
-{
-    Console.WriteLine("Ålder:");
-    string age = Console.ReadLine();
-
-    success = int.TryParse(age, out ageNum);
-
-    if (success == false)
+    string VadSkaManKöpa ="";
+    while (VadSkaManKöpa != "1" && VadSkaManKöpa != "2" && VadSkaManKöpa != "3")
     {
-        Console.WriteLine("Skriv en siffra, malaka");
+        VadSkaManKöpa = Console.ReadLine();
+    }
+
+    int PrisPerSak = 0;
+
+    if (VadSkaManKöpa == "1")
+    {
+        PrisPerSak = 10;
+    }
+    else if (VadSkaManKöpa == "2")
+    {
+        PrisPerSak = 20;
+    }
+    else if (VadSkaManKöpa == "3")
+    {
+        PrisPerSak = 30;
+    }
+
+    Console.WriteLine("Hur många vill du köpa?");
+    int numAttKöpa = 0;
+    while (numAttKöpa == 0)
+    {
+        string num = Console.ReadLine();
+        bool success = int.TryParse(num, out numAttKöpa);
+        if (success == false)
+        {
+            Console.WriteLine("Du måste skriva en siffra");
+        }
+    }
+
+    int SlutKostnaden = PrisPerSak * numAttKöpa;
+
+    if (SlutKostnaden <= SpelarPengar)
+    {
+        Console.WriteLine("Ja, var de bra så?");
+        SpelarPengar -= SlutKostnaden;
+    }
+    else
+    {
+        Console.WriteLine("Du har inte råd med detta!");
     }
 }
 
-if (ageNum >= 18)
-{
-    Console.WriteLine("Välkommen");
-}
-
+Console.Write("Nu har du slut med pengar");
+Console.WriteLine(" Tryck på ENTER för att avsluta");
 Console.ReadLine();
